1a506d91bb7d27eee528c85ea14d5fb2d26e50fb
minwoo0818 <145524060+minwoo0818@users.noreply.github.com>
2025-12-03 15:28:57 +0900

    테일윈드 파일 생성 안되서 수동 구현

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // 쓰던 색상들을 이름으로 지정
+        hd: {
+          primary: '#002c5f',   // 현대차 다크 네이비 (로고색?)
+          blue: '#007bff',      // 기본 버튼 파랑
+          green: '#28a745',     // 엑셀/성공 녹색
+          red: '#dc3545',       // 삭제/경고 빨강
+          gray: '#f4f7f9',      // 배경 회색
+        }
+      }
+    },
+  },
+  plugins: [],
+}
